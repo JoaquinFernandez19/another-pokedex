c2633f9803c016fca476aa325486e9e2f4ee556c
joaquin.fernandez <joaquin.fernandez@suitedb.com>
2023-03-06 15:29:24 -0300

    Add framer motion to include simple fade in for now

--- components/main-content/PokeCard.tsx
+++ components/main-content/PokeCard.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 import React, { useEffect, useRef, useState } from "react";
-import { fetchPokemons, preLoadImgs } from "../../utils/Utils";
+import { preLoadImgs } from "../../utils/Utils";
 import { Pokemon } from "../Types";
 import { Randomizer } from "./Randomizer";
-
+import { motion } from "framer-motion";
 const CREDIT_LIMITS = 100;
 //Undefined bc at load we dont have data yet
 interface PokeCardprops {
@@ -15,7 +14,7 @@ export const PokeCard: React.FC<PokeCardprops> = ({ pokemonList }) => {
   const currPokIndex = useRef<number>(0);
 
   const [currPokemon, setPokemon] = useState<Pokemon>(pokemonList[currPokIndex.current]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const randomize = () => {
     if (credits > 0) setCredits(credits - 1);
   };
@@ -25,22 +24,21 @@ export const PokeCard: React.FC<PokeCardprops> = ({ pokemonList }) => {
     currPokIndex.current++;
     setLoading(true);
     setPokemon(pokemonList[currPokIndex.current]);
+
+    return () => {};
   }, [credits]);
-  //
+
   useEffect(() => {
     const preLoadImgEffect = () => {
-      preLoadImgs(
-        pokemonList.map((pk) => {
-          return pk.img;
-        })
-      );
+      preLoadImgs(pokemonList.map((pk) => pk.img));
     };
     preLoadImgEffect();
+    return () => {};
   }, []);
 
   return (
     <div className="relative">
-      <div className={`${loading ? "invisible" : "visible"}`}>
+      <motion.div animate={{ opacity: [0, 1] }} key={`${currPokemon.id}`}>
         <h1 className="text-center text-3xl text-white mb-10 pokefont">{currPokemon.name}</h1>
         <div className=" relative">
           <img
@@ -48,14 +46,12 @@ export const PokeCard: React.FC<PokeCardprops> = ({ pokemonList }) => {
             alt={currPokemon.name}
             width={400}
             height={400}
-            className="m-auto "
+            className={`m-auto`}
             onLoad={() => setLoading(false)}
           />
         </div>
-      </div>
-      <div className={`${!loading ? "hidden" : "hidden"} absolute top-0`}>
-        <h1 className="text-5xl">Loading...</h1>
-      </div>
+      </motion.div>
+
       <Randomizer usageLimits={`${credits}`} trigger={randomize} />
     </div>
   );
