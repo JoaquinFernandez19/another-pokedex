5af56090e09069c4f9a847163a5edb33bc4a6ed9
JoaquinFernandez19 <copako12@gmail.com>
2023-03-12 13:48:54 -0300

    Fix roll problem in mobile

--- components/main-content/pokecard-components/PokeStats.tsx
+++ components/main-content/pokecard-components/PokeStats.tsx
@@ -47,9 +47,11 @@ export const PokeStats: React.FC<PropsStats> = ({
       initial={"hidden"}
       animate={showStats ? "open" : ""}
       variants={variants}
-      className="z-0 w-auto text-center h-min relative md:text-right md:top-[15%] md:left-[-10%] "
+      className={`${
+        !showStats ? "pointer-events-none" : ""
+      } w-auto text-center h-min relative md:text-right md:top-[15%] md:left-[-10%]`}
     >
-      <div className=" p-3 px-5 flex flex-col gap-1 md:gap-2  stats-circular-info  relative ">
+      <div className="p-3 px-5 flex flex-col gap-1 md:gap-2  stats-circular-info  relative ">
         <motion.div
           className="absolute w-[35%] h-full bottom-0 right-0 m-auto left-0 border-t-0 border-l-0 border-r-0 border-b-[3px] md:border-b-0 md:w-full md:border-r-[3px]"
           style={{
