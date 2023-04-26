3f0a69d26876b10358aa6bf09868943b097795e6
JoaquinFernandez19 <copako12@gmail.com>
2023-04-26 10:02:25 -0300

    database initial

--- pokecollect/app/components/content/login/GoogleLogin.tsx
+++ pokecollect/app/components/content/login/GoogleLogin.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { auth } from "../login/Firebase";
 import { signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { SignoutIcon } from "./SignoutIcon";
+import { generateSessionData } from "../db/DataBase";
 
 export const GoogleLogin: React.FC = ({}) => {
   const googleAuth = new GoogleAuthProvider();
@@ -11,6 +12,15 @@ export const GoogleLogin: React.FC = ({}) => {
     if (user) return;
     signInWithPopup(auth, googleAuth);
   };
+
+  useEffect(() => {
+    if (user) {
+      generateSessionData(user.uid);
+    }
+
+    return () => {};
+  }, [user]);
+
   let text = user ? "Welcome " + user.displayName : "Login with Google";
   if (!loading) {
     return (
