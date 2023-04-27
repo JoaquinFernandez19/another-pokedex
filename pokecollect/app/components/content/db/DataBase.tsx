7fb12fb0e0f46f9dad41ed16c434d4bbf821bc9c
joaquin.fernandez <joaquin.fernandez@suitedb.com>
2023-04-27 09:36:38 -0300

    main

--- pokecollect/app/components/content/db/DataBase.tsx
+++ pokecollect/app/components/content/db/DataBase.tsx
@@ -1,6 +1,6 @@
 import { Pokemon } from "@/app/utils/Types";
 import { db } from "../login/Firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, collection, query, where, getDocs } from "firebase/firestore";
 
 export const generateSessionData = async (userId: string) => {
   const correctedUserData = {};
@@ -20,9 +20,29 @@ const fetchCurrentUserInfo = async (userId: string) => {
   }
 };
 
-const fetchCurrentUserPokemons = (pokemons: string[] = []) => {
-  const arr = [];
+//TODO: dividie the array in parts and fetch by 5 or something like that
+//Return that something is loading to show empty pokemons loading icons
+//if not loading, we onyl show the alredy loaded pokemons!!!
+//Will be hard but no impossible!!!!
+const fetchCurrentUserPokemons = async (pokemons: { pokemon_id: number }[] = []) => {
+  const collectionRef = collection(db, "pokemons");
+  //Get the doc keys
+  const arr: Pokemon[] = [];
+  const docKeys: string[] = [];
   pokemons.forEach((pk) => {
-    ``;
+    docKeys.push(String(pk.pokemon_id));
   });
+  //fetch the docs
+  const docs = await Promise.all(
+    docKeys.map(async (docKey) => {
+      const docRef = doc(collectionRef, docKey);
+      const docSnapshot = await getDoc(docRef);
+      if (!docSnapshot.exists()) {
+        return null;
+      }
+      return docSnapshot.data().info_json;
+    })
+  );
+
+  return arr;
 };
